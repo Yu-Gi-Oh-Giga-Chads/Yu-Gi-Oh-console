65fa6b73ade42910b7a052e24a619807f348b828
Danail73 <dani.marinov.dm@gmail.com>
2024-05-19 16:05:24 +0300

    Add project files.

---
+++ YuGiOh!_MasterDuel/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace YuGiOh__MasterDuel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
